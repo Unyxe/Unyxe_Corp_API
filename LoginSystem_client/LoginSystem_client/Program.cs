0677492eeb4742d1400d7e45249ef1d88546bf1e
Unyxe <114250878+Unyxe@users.noreply.github.com>
2023-04-19 09:33:52 +0100

    Add files via upload

---
+++ LoginSystem_client/LoginSystem_client/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace LoginSystem_server
+{
+    internal class Program
+    {
+        static Socket socket;
+        static IPAddress ip_addr;
+        static IPEndPoint endPoint;
+        static public string ips;
+
+
+        static IPAddress ip2;
+        static IPEndPoint endPoint2;
+        static Thread listenThread;
+
+
+        static int opp_dec;
+        static public string last_msg = "";
+        static Random rand = new Random();
+        static int listening_port = rand.Next() % 30000 + 3000;
+
+        public static HttpClient http_client = new HttpClient()
+        {
+            //BaseAddress = new Uri("http://unyxe.mywire.org"),
+            BaseAddress = new Uri("http://" + GetLocalIPAddress() + ":8080"),
+        };
+
+
+        public static void Main()
+        {
+            //GetUsernameByIP("");
+            //Console.ReadLine();
+            string s = "";
+
+            while (true)
+            {
+                try
+                {
+                    Send(Console.ReadLine() + "~" + listening_port);
+                }
+                catch { }
+            }
+
+        }
+
+        public static void Send(string str)
+        {
+            SendHttp(ToBase64(str));
+        }
+        public static void SendHttp(string str)
+        {
+            HttpResponseMessage response = http_client.GetAsync(str).GetAwaiter().GetResult();
+            string g = response.Headers.Pragma.ToString();
+            var l = response.Headers.ToList();
+            last_msg = FromBase64(l[0].Value.First());
+
+            Console.WriteLine(last_msg);
+            Console.WriteLine("___________________________________");
+        }
+        public static void SendToServer(string str)
+        {
+            Send(str + "~" + listening_port);
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "255.255.255.255";
+        }
+
+        public static string ToBase64(string input)
+        {
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(input));
+        }
+
+        public static string ToBase64FromByte(byte[] input)
+        {
+            return Convert.ToBase64String(input);
+        }
+        public static byte[] FromBase64ToByte(string input)
+        {
+            return Convert.FromBase64String(input);
+        }
+
+        public static string FromBase64(string input)
+        {
+            return Encoding.Default.GetString(Convert.FromBase64String(input));
+        }
+    }
+}
---
+++ LoginSystem_client/LoginSystem_client/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoginSystem_client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lincoln UTC")]
+[assembly: AssemblyProduct("LoginSystem_client")]
+[assembly: AssemblyCopyright("Copyright © Lincoln UTC 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d6d5534-6ebb-4e60-9351-7c66943e13e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
